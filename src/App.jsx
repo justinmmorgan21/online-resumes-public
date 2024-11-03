7a88ac997eb4379a2501e81dff53d4fe9a8f7da3
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-03 11:54:22 -0600

    add students index with dummy student

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import axios from 'axios';
 import { Header } from "./Header";
 import { StudentsPage } from "./StudentsPage";
 import { Footer } from "./Footer";
 
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <StudentsPage />,
+        loader: ()=> 
+          [
+            {
+              //dummy data goes here
+              first_name: "bob"
+            }
+          ]
+          // axios.get("http://localhost:3000/students.json").then(response => response.data)
+      }
+    ]
+  }
+])
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <StudentsPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- src/StudentsPage.jsx
+++ src/StudentsPage.jsx
@@ -1,7 +1,14 @@
+import { useLoaderData } from "react-router-dom"
 export function StudentsPage() {
+
+  const students = useLoaderData();
+
+  console.log(students);
+
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <h1>All Students</h1>
+      <p>{students[0].first_name}</p>
     </main>
   )
 }
