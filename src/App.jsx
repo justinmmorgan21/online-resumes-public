0c8a0ef59065375f4bc6b66d94e96d29d1b65097
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-03 11:31:42 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { StudentsPage } from "./StudentsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <StudentsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/StudentsPage.jsx
@@ -0,0 +1,7 @@
+export function StudentsPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
